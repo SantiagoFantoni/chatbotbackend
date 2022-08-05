9e525b20e5774ea1a0662acfeace1c39d8d37c1e
Santiago Fantoni <fantonisantiago@gmail.com>
2022-08-05 17:21:51 -0300

    setup primer intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
+const { WebhookClient } = require("dialogflow-fulfillment");
 const mongoose = require("mongoose");
 const Staff = require("./models/Staff");
 
@@ -16,12 +17,31 @@ async function main() {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.post("/", async (req, res) => {
-	await Staff.create({
-		firstName: "Sandra",
-		lastName: "Gonzales",
-	});
-	res.json({ message: "Hola" });
+app.post("/webhook", async (req, res) => {
+	const agent = new WebhookClient({ request: req, response: res });
+	console.log("Dialogflow Request headers: " + JSON.stringify(request.headers));
+	console.log("Dialogflow Request body: " + JSON.stringify(request.body));
+
+	function welcome(agent) {
+		agent.add(`Welcome to my agent!`);
+	}
+
+	function fallback(agent) {
+		agent.add(`I didn't understand`);
+		agent.add(`I'm sorry, can you try again?`);
+	}
+
+	function Agendar(agent) {
+		agent.add(`Cual es tu nombre?(desde el webhook)`);
+	}
+
+	let intentMap = new Map();
+	intentMap.set("Default Welcome Intent", welcome);
+	intentMap.set("Default Fallback Intent", fallback);
+	intentMap.set("Agendar", Agendar);
+	// intentMap.set('your intent name here', yourFunctionHandler);
+	// intentMap.set('your intent name here', googleAssistantHandler);
+	agent.handleRequest(intentMap);
 });
 
 app.listen(PORT, () => {
